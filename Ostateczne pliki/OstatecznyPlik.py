82606637e3dd49b24297a2be61932769319cd8ad
Filip <filip.kedzior6@gmail.com>
2026-01-20 20:10:12 +0100

    Initial commit

---
+++ Ostateczne pliki/OstatecznyPlik.py
@@ -0,0 +1,329 @@
+from PySide2 import QtCore, QtWidgets
+import Metashape
+import glob
+import math
+from SkryptUi import Ui_WtykaFTP
+import os
+
+# Checking compatibility
+compatible_major_version = "2.2"
+found_major_version = ".".join(Metashape.app.version.split('.')[:2])
+if found_major_version != compatible_major_version:
+    raise Exception("Incompatible Metashape version: {} != {}".format(found_major_version, compatible_major_version))
+
+class MojaAplikacja(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_WtykaFTP()
+        self.ui.setupUi(self)
+
+        self.doc = Metashape.app.document
+        if len(self.doc.chunks) > 0:
+            self.chunk = self.doc.chunks[0]
+        else:
+            self.chunk = self.doc.addChunk()
+
+        self.ui.photosButton.clicked.connect(self.wybierz_folder_zdjec)
+        self.ui.geodNetButton.clicked.connect(self.wybierz_plik_osnowy)
+        self.ui.runButton.clicked.connect(self.uruchom_proces)
+
+        #konfiguracja
+        self.required_green_markers = 3
+        self.required_photos_measured = 3
+        self.check_interval = 2000
+
+        self.advanced_monitor_timer = QtCore.QTimer()
+        self.msg = QtWidgets.QMessageBox()
+
+    def wybierz_folder_zdjec(self):
+        folder = QtWidgets.QFileDialog.getExistingDirectory(self, "Wybierz folder ze zdjęciami")
+        if folder:
+            self.ui.filePhotosEdit.setText(folder)
+
+    def wybierz_plik_osnowy(self):
+        plik, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Wybierz plik osnowy", "",
+                                              "Pliki tekstowe (*.txt *.csv);;Wszystkie pliki (*.*)")
+        if plik:
+            self.ui.fileEditLine2.setText(plik)
+
+    def uruchom_proces(self):
+        print("Uruchomiono proces!")
+        self.sfmImageProcessing()
+        self.przetwarzankoZDJ()
+
+    def getAllImagesList(self, path):
+        import os
+        search_path = os.path.join(path, "*.jpg")
+        files_jpg = glob.glob(search_path)
+        return files_jpg
+
+    def sfmImageProcessing(self):
+        path = self.ui.filePhotosEdit.text()
+
+        if not path:
+            print("Nie wybrano ścieżki!")
+            return
+
+        image_list = self.getAllImagesList(path)
+
+        print(f"Znaleziono zdjęć: {len(image_list)}")
+        print(image_list)
+
+        if image_list:
+            self.chunk.addPhotos(image_list, load_xmp_orientation=True)
+        else:
+            print("Lista zdjęć jest pusta.")
+
+    def tellQualityInt(self, quality_type):
+        if quality_type == "Ultra low": return 8
+        elif quality_type == "Low": return 4
+        elif quality_type == "Medium": return 2
+        elif quality_type == "High": return 1
+        elif quality_type == "Ultra high": return 0.5
+        else: return ValueError("Nieznana jakość")
+
+    def merge_markers(self):
+        detected_markers = []
+        reference_markers = []
+        merged_dict = {}
+
+        used_reference_labels = set()
+
+        for marker in self.chunk.markers:
+            if marker.label.startswith("point"):
+                detected_markers.append(marker)
+            else:
+                reference_markers.append(marker)
+
+        for marker in detected_markers:
+            if marker.position:
+                pos_internal = marker.position
+                pos_geodetic = self.chunk.crs.project(self.chunk.transform.matrix.mulp(pos_internal))
+
+                xd = pos_geodetic.x
+                yd = pos_geodetic.y
+                zd = pos_geodetic.z
+
+                dist = []
+                for r_marker in reference_markers:
+                    if r_marker.label in used_reference_labels:
+                        continue
+                    if not r_marker.reference.location:
+                        continue
+
+                    source_location_r = r_marker.reference.location
+                    xr = source_location_r.x
+                    yr = source_location_r.y
+                    zr = source_location_r.z
+
+                    odl = math.sqrt((xr - xd) ** 2 + (yr - yd) ** 2 + (zr - zd) ** 2)
+                    dist.append((odl, r_marker.label))
+
+                if dist:
+                    lowest = min(dist)
+                    min_dist = lowest[0]
+                    best_match_label = lowest[1]
+
+                    merged_dict[marker.label] = best_match_label
+                    used_reference_labels.add(best_match_label)
+
+                    print(f"Dopasowano: {marker.label} -> {best_match_label} (dist: {min_dist:.3f})")
+                else:
+                    print(f"Dla {marker.label} nie znaleziono wolnego markera referencyjnego.")
+
+        markers_by_name = {m.label: m for m in self.chunk.markers}
+
+        for detected_name, reference_name in merged_dict.items():
+            det_marker = markers_by_name.get(detected_name)
+            ref_marker = markers_by_name.get(reference_name)
+            if det_marker and ref_marker:
+                for camera, projection in det_marker.projections.items():
+                    ref_marker.projections[camera] = projection
+                ref_marker.enabled = True
+
+                self.chunk.remove(det_marker)
+
+        self.chunk.updateTransform()
+
+    def export_eo(self):
+        photo_path = self.ui.filePhotosEdit.text()
+        path = os.path.join(photo_path, "exteriorOrientation.txt")
+
+        self.chunk.exportReference(
+            path=path,
+            format=Metashape.ReferenceFormatCSV,
+            items=Metashape.ReferenceItemsCameras,
+            columns="nuvwdef",  # n=Label, u/v/w=estimated coordinates, d/e/f=estimated coordination angles
+            delimiter=",",
+            precision=6
+        )
+
+        print(f"Zapisano orientację do: {path}")
+
+    def import_r_markers(self):
+        geonet_path = self.ui.fileEditLine2.text()
+        epsg_geo_net = self.ui.coordsEdit2.text()
+        epsg_geo_net_full = f"EPSG::{epsg_geo_net}"
+        crs_geo_net = Metashape.CoordinateSystem(epsg_geo_net_full)
+
+        self.chunk.importReference(
+            path=geonet_path,
+            format=Metashape.ReferenceFormatCSV,
+            columns="nyxz",
+            delimiter="\t",
+            crs=crs_geo_net,
+            skip_rows=0,
+            ignore_labels=False,
+            create_markers=True
+        )
+
+        return len(self.chunk.markers)
+
+    def check_greens(self):
+        fully_green = 0
+        for marker in self.chunk.markers:
+            if not marker.projections:
+                continue
+
+            green_on_this_marker = 0
+            for camera, projection in marker.projections.items():
+                if projection.pinned:
+                    green_on_this_marker += 1
+
+            if green_on_this_marker >= self.required_photos_measured:
+                fully_green += 1
+
+        if fully_green >= self.required_green_markers:
+            self.advanced_monitor_timer.stop()
+            self.msg.setWindowTitle("Czas decyzji")
+            self.msg.setText("Czy chcesz kontynuować?\nTak - uruchomienie kolejnych etapów.\nNie - możliwość pomiaru dalszych punktów.")
+            self.msg.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+            self.msg.setDefaultButton(QtWidgets.QMessageBox.Yes)
+            result = self.msg.exec_()
+
+            if result == QtWidgets.QMessageBox.No:
+                self.advanced_monitor_timer.start(self.check_interval)
+                self.required_green_markers += 1
+            elif result == QtWidgets.QMessageBox.Yes:
+                self.chunk.optimizeCameras(
+                    fit_f=True, fit_cx=True, fit_cy=True,
+                    fit_k1=True, fit_k2=True, fit_k3=True,
+                    fit_p1=True, fit_p2=True, adaptive_fitting=True)
+
+                self.next_part()
+
+    def przetwarzankoZDJ(self):
+        accuracy_match_photos = self.ui.chooseQualityOrient.currentText()
+
+        epsg_photos = self.ui.coordsEdit1.text()
+        epsg_end = self.ui.coordsEdit3.text()
+
+        epsg_photos_full = f"EPSG::{epsg_photos}"
+        epsg_end_full = f"EPSG::{epsg_end}"
+
+        crs_photos = Metashape.CoordinateSystem(epsg_photos_full)
+        crs_end = Metashape.CoordinateSystem(epsg_end_full)
+        ####################################################################
+        
+        if epsg_photos != epsg_end:
+            for camera in self.chunk.cameras:
+                if not camera.reference.location: continue
+                camera_location = camera.reference.location
+                final_camera_location = Metashape.CoordinateSystem.transform(camera_location, crs_photos, crs_end)
+                camera.reference.location = final_camera_location
+
+        self.chunk.crs = crs_end
+        self.chunk.camera_crs = crs_end
+
+        Metashape.app.update()
+
+        self.chunk.matchPhotos(downscale=self.tellQualityInt(accuracy_match_photos), generic_preselection=False, reference_preselection=False)
+        self.chunk.alignCameras()
+
+        length = self.import_r_markers()
+        markers_copy = list(self.chunk.markers)
+
+        for marker in markers_copy:
+            self.chunk.remove(marker)
+
+        self.chunk.detectMarkers(target_type=Metashape.TargetType.CrossTarget, tolerance=10)
+
+        while len(self.chunk.markers) > length:
+            last_marker = self.chunk.markers[-1]
+
+            self.chunk.remove(last_marker)
+
+        for marker in self.chunk.markers:
+            for camera, projection in marker.projections.items():
+                projection.pinned = False
+                marker.projections[camera] = projection
+
+        self.advanced_monitor_timer.timeout.connect(self.check_greens)
+        self.advanced_monitor_timer.start(self.check_interval)
+
+        self.check_greens()
+
+    def next_part(self):
+        accuracy_point_cloud = self.ui.chooseQualityCloudPoint.currentText()
+        accuracy_model_3d = self.ui.chooseQualityModel.currentText()
+        epsg_geo_net = self.ui.coordsEdit2.text()
+        epsg_end = self.ui.coordsEdit3.text()
+        epsg_geo_net_full = f"EPSG::{epsg_geo_net}"
+        epsg_end_full = f"EPSG::{epsg_end}"
+        crs_geo_net = Metashape.CoordinateSystem(epsg_geo_net_full)
+        crs_end = Metashape.CoordinateSystem(epsg_end_full)
+
+        _ = self.import_r_markers()
+
+        if epsg_geo_net != epsg_end:
+            for marker in self.chunk.markers:
+                if not marker.label.startswith("point"):
+                    if not marker.reference.location:
+                        continue
+                    marker_location = marker.reference.location
+                    final_marker_location = Metashape.CoordinateSystem.transform(marker_location, crs_geo_net, crs_end)
+                    marker.reference.location = final_marker_location
+
+        self.chunk.marker_crs = crs_end
+
+        self.merge_markers()
+
+        self.export_eo()
+
+        if self.ui.modelBox.isChecked():
+            if accuracy_point_cloud == accuracy_model_3d:
+                self.chunk.buildDepthMaps(downscale=self.tellQualityInt(accuracy_point_cloud), filter_mode=Metashape.AggressiveFiltering)
+                if self.ui.cloudPointBox.isChecked():
+                    self.chunk.buildPointCloud()
+                self.chunk.buildModel(source_data=Metashape.DepthMapsData, surface_type=Metashape.Arbitrary,interpolation=Metashape.EnabledInterpolation)
+                self.chunk.buildUV(mapping_mode=Metashape.GenericMapping)
+            else:
+                if self.ui.cloudPointBox.isChecked():
+                    self.chunk.buildDepthMaps(downscale=self.tellQualityInt(accuracy_point_cloud), filter_mode=Metashape.AggressiveFiltering)
+                    self.chunk.buildPointCloud()
+                self.chunk.buildDepthMaps(downscale=self.tellQualityInt(accuracy_model_3d),filter_mode=Metashape.AggressiveFiltering)
+                self.chunk.buildModel(source_data=Metashape.DepthMapsData, surface_type=Metashape.Arbitrary, interpolation=Metashape.EnabledInterpolation)
+                self.chunk.buildUV(mapping_mode=Metashape.GenericMapping)
+
+        #save document 
+        document_name = "wtyka.psx"
+        photo_path = self.ui.filePhotosEdit.text()
+        document_path = os.path.join(photo_path, document_name)
+        self.doc.save(document_path)
+
+        self.close()
+
+app_window = None
+
+def show_window():
+    global app_window
+
+    if app_window is None:
+        app_window = MojaAplikacja()
+
+    app_window.show()
+
+def init_menu():
+    Metashape.app.addMenuItem("FTP", show_window)
+
+init_menu()
