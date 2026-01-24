31e4c48b17b1c67ab2d22c8e8ea3fff6a48d21d1
Filip <filip.kedzior6@gmail.com>
2026-01-24 22:38:16 +0100

    DZIALA

--- Ostateczne pliki/OstatecznyPlik.py
+++ Ostateczne pliki/OstatecznyPlik.py
@@ -13,8 +13,7 @@ found_major_version = ".".join(Metashape.app.version.split('.')[:2])
 if found_major_version != compatible_major_version:
     raise Exception("Incompatible Metashape version: {} != {}".format(found_major_version, compatible_major_version))
 
-
-class MojaAplikacja(QtWidgets.QMainWindow):
+class MyApp(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_WtykaFTP()
@@ -188,6 +187,108 @@ class MojaAplikacja(QtWidgets.QMainWindow):
 
         return len(self.chunk.markers)
 
+    def count_ref_markers(self):
+        geonet_path = self.ui.fileEditLine2.text()
+        marker_quantity = 0
+        with open(geonet_path, 'r') as f:
+            for line in f:
+                if line.strip():
+                    marker_quantity += 1
+        return marker_quantity
+
+    def detect_rest_from_fast(self):
+        """
+        #Problem jest taki, że Metashape w wersji demo nie da rady tego chyba zapisać, dlatego będzie to ręcznie obsłużone
+        #aby można było sprawdzić czy działa
+
+        path = os.path.join(photo_path, "in_orient.xml")
+        sensor = self.chunk.sensors[0]
+        sensor.calibration.save(path, format=Metashape.CalibrationFormat.CalibrationFormatOpenCV)
+
+        fs = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
+        K = fs.getNode("camera_matrix").mat()
+        dist = fs.getNode("distortion_coefficients").mat()
+        fs.release()
+        """
+        print("Wykrywanie pozostałych punktów...")
+        sensor = self.chunk.sensors[0]
+        calib = sensor.calibration
+
+        width = sensor.width
+        height = sensor.height
+
+        K = np.array([
+            [calib.f, 0, calib.cx + width / 2],
+            [0, calib.f, calib.cy + height / 2],
+            [0, 0, 1]
+        ], dtype=np.float32)
+
+        dist = np.array([calib.k1, calib.k2, calib.p1, calib.p2, calib.k3], dtype=np.float32)
+
+        fast = cv2.FastFeatureDetector_create()
+        fast.setThreshold(60)
+        fast.setNonmaxSuppression(True)
+
+        for marker in self.chunk.markers:
+            geo_location = marker.reference.location
+            loc_internal = self.chunk.crs.unproject(geo_location)
+
+            xyz = np.array([loc_internal.x, loc_internal.y, loc_internal.z], dtype=np.float32)
+            for camera in self.chunk.cameras:
+                if not camera.transform:
+                    continue
+                m = camera.transform.inv()
+                rotation_list = np.array([
+                    [m[0, 0], m[0, 1], m[0, 2]],
+                    [m[1, 0], m[1, 1], m[1, 2]],
+                    [m[2, 0], m[2, 1], m[2, 2]]
+                ], dtype=np.float32)
+
+                translation_list = np.array([m[0, 3], m[1, 3], m[2, 3]], dtype=np.float32).reshape(-1, 1)
+                R, _ = cv2.Rodrigues(rotation_list)
+
+                margin = 30
+
+                points_2d, _ = cv2.projectPoints(xyz, R, translation_list, K, dist)
+                proj_x, proj_y = points_2d[0][0]
+
+                if not (margin < proj_x < sensor.width - margin and margin < proj_y < sensor.height - margin):
+                    continue
+
+                path = camera.photo.path
+                img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+                x_start = int(proj_x - margin)
+                y_start = int(proj_y - margin)
+                x_end = int(proj_x + margin)
+                y_end = int(proj_y + margin)
+
+                roi = img[y_start:y_end, x_start:x_end]
+                kp = fast.detect(roi, None)
+
+                best_kp = None
+                min_dist_local = float('inf')
+
+                center_roi_x, center_roi_y = margin, margin
+
+                for k in kp:
+                    dx = k.pt[0] - center_roi_x
+                    dy = k.pt[1] - center_roi_y
+                    d = math.sqrt(dx * dx + dy * dy)
+                    if d < min_dist_local:
+                        min_dist_local = d
+                        best_kp = k
+
+                if best_kp and min_dist_local < 15:
+                    final_x = x_start + best_kp.pt[0]
+                    final_y = y_start + best_kp.pt[1]
+
+                    marker.projections[camera] = Metashape.Marker.Projection(Metashape.Vector([final_x, final_y]), pinned=True)
+
+        self.chunk.optimizeCameras(fit_f=True, fit_cx=True, fit_cy=True, fit_k1=True, fit_k2=True, fit_k3=True,
+                                   fit_p1=True, fit_p2=True)
+        self.chunk.updateTransform()
+
     def check_greens(self):
         fully_green = 0
         for marker in self.chunk.markers:
@@ -220,31 +321,13 @@ class MojaAplikacja(QtWidgets.QMainWindow):
                     fit_k1=True, fit_k2=True, fit_k3=True,
                     fit_p1=True, fit_p2=True, adaptive_fitting=True)
 
+                self.chunk.updateTransform()
+
                 self.next_part()
 
     def przetwarzankoZDJ(self):
         accuracy_match_photos = self.ui.chooseQualityOrient.currentText()
-
-        epsg_photos = self.ui.coordsEdit1.text()
-        epsg_end = self.ui.coordsEdit3.text()
-
-        epsg_photos_full = f"EPSG::{epsg_photos}"
-        epsg_end_full = f"EPSG::{epsg_end}"
-
-        crs_photos = Metashape.CoordinateSystem(epsg_photos_full)
-        crs_end = Metashape.CoordinateSystem(epsg_end_full)
         ####################################################################
-
-        if epsg_photos != epsg_end:
-            for camera in self.chunk.cameras:
-                if not camera.reference.location: continue
-                camera_location = camera.reference.location
-                final_camera_location = Metashape.CoordinateSystem.transform(camera_location, crs_photos, crs_end)
-                camera.reference.location = final_camera_location
-
-        self.chunk.crs = crs_end
-        self.chunk.camera_crs = crs_end
-        self.chunk.marker_crs = crs_end
 
         Metashape.app.update()
 
@@ -252,18 +335,19 @@ class MojaAplikacja(QtWidgets.QMainWindow):
                                reference_preselection=False)
         self.chunk.alignCameras()
 
-        length = self.import_r_markers()
-        markers_copy = list(self.chunk.markers)
-
-        for marker in markers_copy:
-            self.chunk.remove(marker)
+        length = self.count_ref_markers()
 
         self.chunk.detectMarkers(target_type=Metashape.TargetType.CrossTarget, tolerance=10)
 
-        while len(self.chunk.markers) > length:
-            last_marker = self.chunk.markers[-1]
+        current_markers = list(self.chunk.markers)
 
-            self.chunk.remove(last_marker)
+        if len(current_markers) > length:
+            current_markers.sort(key=lambda m: len(m.projections))
+            to_remove_count = len(current_markers) - length
+            markers_to_remove = current_markers[:to_remove_count]
+
+            for marker in markers_to_remove:
+                self.chunk.remove(marker)
 
         for marker in self.chunk.markers:
             for camera, projection in marker.projections.items():
@@ -279,12 +363,17 @@ class MojaAplikacja(QtWidgets.QMainWindow):
         accuracy_point_cloud = self.ui.chooseQualityCloudPoint.currentText()
         accuracy_model_3d = self.ui.chooseQualityModel.currentText()
         epsg_geo_net = self.ui.coordsEdit2.text()
-        epsg_end = self.ui.coordsEdit3.text()
         epsg_geo_net_full = f"EPSG::{epsg_geo_net}"
-        epsg_end_full = f"EPSG::{epsg_end}"
         crs_geo_net = Metashape.CoordinateSystem(epsg_geo_net_full)
-        crs_end = Metashape.CoordinateSystem(epsg_end_full)
         photo_path = self.ui.filePhotosEdit.text()
+        epsg_photos = self.ui.coordsEdit1.text()
+        epsg_end = self.ui.coordsEdit3.text()
+
+        epsg_photos_full = f"EPSG::{epsg_photos}"
+        epsg_end_full = f"EPSG::{epsg_end}"
+
+        crs_photos = Metashape.CoordinateSystem(epsg_photos_full)
+        crs_end = Metashape.CoordinateSystem(epsg_end_full)
 
         _ = self.import_r_markers()
 
@@ -297,45 +386,19 @@ class MojaAplikacja(QtWidgets.QMainWindow):
                     final_marker_location = Metashape.CoordinateSystem.transform(marker_location, crs_geo_net, crs_end)
                     marker.reference.location = final_marker_location
 
+        if epsg_photos != epsg_end:
+            for camera in self.chunk.cameras:
+                if not camera.reference.location: continue
+                camera_location = camera.reference.location
+                final_camera_location = Metashape.CoordinateSystem.transform(camera_location, crs_photos, crs_end)
+                camera.reference.location = final_camera_location
+
+        self.chunk.crs = crs_end
+        self.chunk.camera_crs = crs_end
+        self.chunk.marker_crs = crs_end
 
         self.merge_markers()
-
-        path = os.path.join(photo_path, "in_orient.xml")
-        sensor = self.chunk.sensors[0]
-        sensor.calibration.save(path, format=Metashape.CalibrationFormat.CalibrationFormatOpenCV)
-
-        fs = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
-        K = fs.getNode("camera_matrix").mat()
-        dist = fs.getNode("distortion_coefficients").mat()
-        fs.release()
-
-        nr = []
-        xyz = []
-
-        for marker in self.chunk.markers:
-            nr.append(marker.label)
-            x = marker.reference.location.x
-            y = marker.reference.location.y
-            z = marker.reference.location.z
-            xyz.append([x, y, z])
-
-        xyz = np.array(xyz, dtype=np.float32)
-
-        camera = self.chunk.cameras[0]
-        m = camera.transform
-
-        rotation_list = np.array([
-            [m[0, 0], m[0, 1], m[0, 2]],
-            [m[1, 0], m[1, 1], m[1, 2]],
-            [m[2, 0], m[2, 1], m[2, 2]]
-        ], dtype=np.float32)
-
-        translation_list = np.array([m[0, 3], m[1, 3], m[2, 3]], dtype=np.float32).reshape(-1, 1)
-
-        R = cv2.Rodrigues(rotation_list)
-
-        points_2d, _ = cv2.projectPoints(xyz, R, translation_list, K, dist)
-
+        self.detect_rest_from_fast()
         self.export_eo()
 
         if self.ui.modelBox.isChecked():
@@ -359,27 +422,23 @@ class MojaAplikacja(QtWidgets.QMainWindow):
                 self.chunk.buildUV(mapping_mode=Metashape.GenericMapping)
 
         # save document
-        document_name = "wtyka.psx"
+        document_name = "wtykaFTP.psx"
         document_path = os.path.join(photo_path, document_name)
         self.doc.save(document_path)
 
         self.close()
 
-
 app_window = None
-
 
 def show_window():
     global app_window
 
     if app_window is None:
-        app_window = MojaAplikacja()
+        app_window = MyApp()
 
     app_window.show()
 
-
 def init_menu():
     Metashape.app.addMenuItem("FTP", show_window)
-
 
 init_menu()
